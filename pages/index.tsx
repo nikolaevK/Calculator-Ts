e22a1cc90287426c467a430a81b1c42e7d242e9b
Konstantin Nikolaev <konstantinnikolaev@Konstantins-MacBook-Pro.local>
2022-11-14 17:56:04 -0800

    Code Clean-up

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,58 +26,60 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col mt-4 mb-4 items-center justify-center py-2">
+    <>
       <Head>
         <title>Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col mx-[40rem]">
-        <div className=" flex justify-center mb-1">
-          <section className="h-[12rem] w-[20.8rem] flex bg-black flex-col items-end justify-between p-3 break-words break-all">
-            <div className="text-white text-4xl">{input}</div>
-            {answer && <div className="text-white text-4xl">{answer}</div>}
+      <div className="flex min-h-screen flex-col mt-4 mb-4 items-center justify-center py-2">
+        <div className="flex flex-col mx-[40rem]">
+          <div className=" flex justify-center mb-1">
+            <section className="h-[12rem] w-[20.8rem] flex bg-black flex-col items-end justify-between p-3 break-words break-all">
+              <div className="text-white text-4xl">{input}</div>
+              {answer && <div className="text-white text-4xl">{answer}</div>}
+            </section>
+          </div>
+          <section className="grid justify-center grid-cols-col4 grid-rows-row5 gap-1">
+            <ExpressionButton value="^" updateExpression={updateExpression} />
+            <button
+              className="button"
+              onClick={() => setInput(input.slice(0, -1))}
+            >
+              AC
+            </button>
+            <button
+              className="button"
+              onClick={() => {
+                setInput("");
+                setAnswer("");
+              }}
+            >
+              Del
+            </button>
+            <ExpressionButton value="/" updateExpression={updateExpression} />
+            <ExpressionButton value="1" updateExpression={updateExpression} />
+            <ExpressionButton value="2" updateExpression={updateExpression} />
+            <ExpressionButton value="3" updateExpression={updateExpression} />
+            <ExpressionButton value="*" updateExpression={updateExpression} />
+            <ExpressionButton value="4" updateExpression={updateExpression} />
+            <ExpressionButton value="5" updateExpression={updateExpression} />
+            <ExpressionButton value="6" updateExpression={updateExpression} />
+            <ExpressionButton value="+" updateExpression={updateExpression} />
+            <ExpressionButton value="7" updateExpression={updateExpression} />
+            <ExpressionButton value="8" updateExpression={updateExpression} />
+            <ExpressionButton value="9" updateExpression={updateExpression} />
+            <ExpressionButton value="-" updateExpression={updateExpression} />
+            <ExpressionButton value="0" updateExpression={updateExpression} />
+            <ExpressionButton value="." updateExpression={updateExpression} />
+            <ExpressionButton value="(" updateExpression={updateExpression} />
+            <ExpressionButton value=")" updateExpression={updateExpression} />
+            <button className="button col-span-2" onClick={onEqualsClick}>
+              =
+            </button>
           </section>
         </div>
-        <section className="grid justify-center grid-cols-col4 grid-rows-row5 gap-1">
-          <ExpressionButton value="^" updateExpression={updateExpression} />
-          <button
-            className="button"
-            onClick={() => setInput(input.slice(0, -1))}
-          >
-            AC
-          </button>
-          <button
-            className="button"
-            onClick={() => {
-              setInput("");
-              setAnswer("");
-            }}
-          >
-            Del
-          </button>
-          <ExpressionButton value="/" updateExpression={updateExpression} />
-          <ExpressionButton value="1" updateExpression={updateExpression} />
-          <ExpressionButton value="2" updateExpression={updateExpression} />
-          <ExpressionButton value="3" updateExpression={updateExpression} />
-          <ExpressionButton value="*" updateExpression={updateExpression} />
-          <ExpressionButton value="4" updateExpression={updateExpression} />
-          <ExpressionButton value="5" updateExpression={updateExpression} />
-          <ExpressionButton value="6" updateExpression={updateExpression} />
-          <ExpressionButton value="+" updateExpression={updateExpression} />
-          <ExpressionButton value="7" updateExpression={updateExpression} />
-          <ExpressionButton value="8" updateExpression={updateExpression} />
-          <ExpressionButton value="9" updateExpression={updateExpression} />
-          <ExpressionButton value="-" updateExpression={updateExpression} />
-          <ExpressionButton value="0" updateExpression={updateExpression} />
-          <ExpressionButton value="." updateExpression={updateExpression} />
-          <ExpressionButton value="(" updateExpression={updateExpression} />
-          <ExpressionButton value=")" updateExpression={updateExpression} />
-          <button className="button col-span-2" onClick={onEqualsClick}>
-            =
-          </button>
-        </section>
       </div>
-    </div>
+    </>
   );
 };
 
